915adfaedb1541b62f9dc9b454f240d25487007f
Ipsita-Kar11 <karipsita1101@gmail.com>
2024-08-21 13:01:10 +0530

    jump

--- Assets/scripts/movement scripts.cs
+++ Assets/scripts/movement scripts.cs
@@ -8,10 +8,30 @@ public class movementscripts : MonoBehaviour
 {
     Vector2 movementInput;
     public  float movementSpeed = 0.1f;
+
+    public float gravity = 9.8f;
+    public float jumpspeed = 20f;
+    public float verticalspeed = 0;
     public void IAmovement(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
         
+    }
+
+    public void IAjump(InputAction.CallbackContext context)
+    {
+        if(context.started==true)
+        {
+            Debug.Log("Started");
+        }
+        if(context.performed == true)
+        {
+            Debug.Log("performed");
+        }
+        if (context.canceled == true)
+        {
+            Debug.Log("canceled");
+        }
     }
     void Start()
     {
@@ -19,9 +39,28 @@ public class movementscripts : MonoBehaviour
     }
     void Update()
     {
-       
-        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, 0, movementInput.y*movementSpeed * Time.deltaTime);
+        if (GroundCheck() == true)
+        {
+            verticalspeed = 0;
+        }
+
+        else
+        {
+            verticalspeed = (verticalspeed - gravity) * Time.deltaTime;
+        }
+
+
+        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, verticalspeed, movementInput.y*movementSpeed * Time.deltaTime);
     }
    
-    
+    public bool GroundCheck()
+    {
+        return Physics.Raycast(transform.position, transform.up * -1, 1);
+    }
+
+   
+
+
+
+
 }
