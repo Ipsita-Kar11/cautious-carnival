893f6971c9cb1654431ec8f15237fbe608637e82
Ipsita-Kar11 <karipsita1101@gmail.com>
2024-08-28 09:16:02 +0530

    interact

--- Assets/scripts/movement scripts.cs
+++ Assets/scripts/movement scripts.cs
@@ -7,31 +7,28 @@ using JetBrains.Annotations;
 public class movementscripts : MonoBehaviour
 {
     Vector2 movementInput;
-    public  float movementSpeed = 0.1f;
-
+    public float movementSpeed = 0.1f;
+    public Cinemachine.CinemachineVirtualCamera playerCamera;
     public float gravity = 9.8f;
     public float jumpspeed = 20f;
     public float verticalspeed = 0;
     public void IAmovement(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
-        
+
     }
 
     public void IAjump(InputAction.CallbackContext context)
     {
-        if(context.started==true)
+        if (context.started == true && GroundCheck())
         {
-            Debug.Log("Started");
+            verticalspeed = jumpspeed;
         }
-        if(context.performed == true)
-        {
-            Debug.Log("performed");
-        }
-        if (context.canceled == true)
-        {
-            Debug.Log("canceled");
-        }
+    }
+
+    public void IAInteract(InputAction.CallbackContext context)
+    {
+
     }
     void Start()
     {
@@ -39,23 +36,35 @@ public class movementscripts : MonoBehaviour
     }
     void Update()
     {
-        if (GroundCheck() == true)
+        if (GroundCheck() == true && verticalspeed <= 0)
         {
             verticalspeed = 0;
         }
 
         else
         {
-            verticalspeed = (verticalspeed - gravity) * Time.deltaTime;
+            verticalspeed = verticalspeed - gravity * Time.deltaTime;
         }
 
 
-        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, verticalspeed, movementInput.y*movementSpeed * Time.deltaTime);
+        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, verticalspeed * Time.deltaTime, movementInput.y*movementSpeed * Time.deltaTime);
     }
    
     public bool GroundCheck()
     {
-        return Physics.Raycast(transform.position, transform.up * -1, 1);
+        return Physics.Raycast(transform.position, transform.up * -1, 1.1f);
+
+       public void InteractionRaycast()
+        {
+        Vector3 myposition = transform.position;
+            Vector3 Cameradirection = playerCamera.transform.forward;
+
+            Ray Interactionray = new Ray(transform.position,Cameradirection);
+
+            RaycastHit targetInfo;
+
+            Physics.Raycast(Interactionray, out targetInfo, 5f);
+        }
     }
 
    
